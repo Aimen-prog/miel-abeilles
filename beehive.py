ff73195b173a1d29836319ae9c883ee3f97e5eba
aimen <cherif_aymenn@hotmail.com>
2023-12-16 15:58:48 +0100

    best bee path updated

--- beehive.py
+++ beehive.py
@@ -2,13 +2,19 @@ import random
 import math
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
+import networkx as nx
+
 
 class Bee:
+    id_counter = 0
     def __init__(self):
         self.flowers = []
         self.fitness = 0
         self.hive = (500, 500)
+        self.parent1 = None
+        self.parent2 = None
+        self.id = Bee.id_counter
+        Bee.id_counter += 1
 
     def get_flowers(self, file):
         df = pd.read_excel(file)
@@ -25,9 +31,10 @@ class Bee:
 class Hive:
     def __init__(self):
         self.beeFitness = {}
-        self.superBees = []
+        self.superBeesFitness = []
         self.child_list = []
-        self.superBees_id = []
+        self.parents_list = []
+        self.super_bees = []
         self.best_bee = None
 
     def getBees(self, file):
@@ -40,23 +47,27 @@ class Hive:
 
     def getSuperBees(self):
         sorted_by_fitness = dict(sorted(self.beeFitness.items(), key=lambda x: x[1]))
-        self.superBees_id = list(sorted_by_fitness.keys())[:50]
-        self.superBees = list(sorted_by_fitness.values())[:50]
+        self.super_bees = list(sorted_by_fitness.keys())[:50]
+        self.superBeesFitness = list(sorted_by_fitness.values())[:50]
 
 
 ### Creation des generations
 
-    def getChild(self, flowers):
+    def getChild(self, flowers, p1, p2):
         child1 = Bee()
         child2 = Bee()
         child1.flowers = flowers[0]
         child2.flowers = flowers[1]
         child1.fly()
         child2.fly()
+        child1.parent1 = p1
+        child2.parent1 = p1
+        child1.parent2 = p2
+        child2.parent2 = p2
         return child1, child2
 
     def evolve_population(self):
-        current_population = self.superBees_id[:]
+        current_population = self.super_bees[:]
         best_bee = min(current_population, key=lambda x: x.fitness)
         if self.best_bee is None or best_bee.fitness < self.best_bee.fitness:
             self.best_bee = best_bee
@@ -68,15 +79,16 @@ class Hive:
             child1_flower_list = parent1.flowers[:math.floor(len(parent1.flowers) / 2)] + [x for x in parent2.flowers if x not in parent1.flowers[:math.floor(len(parent1.flowers) / 2)]]
             child2_flower_list = parent2.flowers[:math.floor(len(parent2.flowers) / 2)] + [x for x in parent1.flowers if x not in parent2.flowers[:math.floor(len(parent2.flowers) / 2)]]
             # Generate child bees using the new flower lists
-            children = self.getChild((child1_flower_list, child2_flower_list))
+            children = self.getChild((child1_flower_list, child2_flower_list), parent1, parent2)
             # Add the children to the next generation
             self.child_list.extend([children[0], children[1]])
         #add the generation to the population
-        self.superBees_id += self.child_list
+        self.super_bees += self.child_list
         self.child_list.clear()
-        fitness_list = [bee.fitness for bee in self.superBees_id]
+        self.parents_list.clear()
+        fitness_list = [bee.fitness for bee in self.super_bees]
         self.beeFitness = {}
-        for bee in self.superBees_id:
+        for bee in self.super_bees:
             self.beeFitness[bee] = bee.fitness
         return fitness_list
 
@@ -88,20 +100,14 @@ class Hive:
         plt.show()
 
 
-    # def add_mutation(self):
-    #     current_population = self.superBees_id
-    #     bee = random.choice(current_population)
-    #     i = random.randint(1, len(bee.flowers) - 1)
-    #     bee.flowers = bee.flowers[i:] + bee.flowers[:i]
-
     def swap_mutation(self):
-        current_population = self.superBees_id
+        current_population = self.super_bees
         bee = random.choice(current_population)
         i, j = random.sample(range(len(bee.flowers)), 2)
         bee.flowers[i], bee.flowers[j] = bee.flowers[j], bee.flowers[i]
 
     def reverse_mutation(self):
-        current_population = self.superBees_id
+        current_population = self.super_bees
         bee = random.choice(current_population)
         i, j = sorted(random.sample(range(len(bee.flowers)), 2))
         bee.flowers[i:j+1] = reversed(bee.flowers[i:j+1])
@@ -109,10 +115,8 @@ class Hive:
     def add_mutation(self):
         mutation_type = random.choice(['swap', 'reverse'])
         if mutation_type == 'swap':
-            print("swap")
             self.swap_mutation()
         elif mutation_type == 'reverse':
-            print("reverse")
             self.reverse_mutation()
 
     def plot_best_bee_path(self):
@@ -128,6 +132,67 @@ class Hive:
         plt.legend()
         plt.show()
 
-    def getBestBeeFitness(self):
-        # print(self.best_bee.fitness)
-        return self.best_bee.fitness
+    # def create_genealogy_tree(self):
+    #     graph = nx.DiGraph()
+    #     current_bee = self.best_bee
+    #     tmp = [current_bee]
+    #     while tmp:
+    #         current_bee = tmp[0]
+    #         graph.add_node(current_bee.id, label=f"Bee {current_bee.id}", color="red")
+    #         if current_bee.parent1:
+    #             graph.add_edge(current_bee.parent1.id, current_bee.id)
+    #
+    #         if current_bee.parent2:
+    #             graph.add_edge(current_bee.parent2.id, current_bee.id)
+    #
+    #         tmp = tmp[1:]
+    #         if current_bee.parent1:
+    #             tmp.append(current_bee.parent1)
+    #         if current_bee.parent2:
+    #             tmp.append(current_bee.parent2)
+    #     pos = nx.spring_layout(graph)
+    #     # Set color for nodes based on the 'color' attribute
+    #     node_colors = ["pink" if node == self.best_bee.id else "skyblue" for node in graph.nodes]
+    #     labels = nx.get_edge_attributes(graph, 'label')
+    #     nx.draw(graph, pos, with_labels=True, node_size=600, node_color=node_colors, font_size=10, font_color="black",
+    #             font_weight="bold")
+    #     nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
+    #     plt.show()
+
+
+
+
+
+    def create_genealogy_tree(self, num_generations):
+        graph = nx.DiGraph()
+        best_bee_history = [self.best_bee]
+
+        for generation in range(num_generations):
+            current_bee = best_bee_history[-1]
+            graph.add_node(current_bee.id, label=f"Bee {current_bee.id}", color="red")
+
+            if current_bee.parent1:
+                graph.add_edge(current_bee.parent1.id, current_bee.id)
+
+            if current_bee.parent2:
+                graph.add_edge(current_bee.parent2.id, current_bee.id)
+
+            if current_bee.parent1:
+                best_bee_history.append(current_bee.parent1)
+
+            if current_bee.parent2:
+                best_bee_history.append(current_bee.parent2)
+
+        pos = nx.spring_layout(graph)
+        # Set color for nodes based on the 'color' attribute
+        node_colors = ["pink" if node == self.best_bee.id else "skyblue" for node in graph.nodes]
+        labels = nx.get_edge_attributes(graph, 'label')
+        nx.draw(graph, pos, with_labels=True, node_size=600, node_color=node_colors, font_size=10, font_color="black",
+                font_weight="bold")
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
+        plt.show()
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from beehive import *
 
+
 def main(num_generations):
     hive = Hive()
     hive.getBees('Champ de pissenlits et de sauge des pres.xlsx')
     fitness_per_generation = []
+
     for gen in range(num_generations):
         hive.getSuperBees()
         if gen % 10 == 0:
@@ -11,12 +13,11 @@ def main(num_generations):
         fitness_list = hive.evolve_population()
         average_fitness = sum(fitness_list) / len(fitness_list)
         fitness_per_generation.append(average_fitness)
-        # hive.getBestBeeFitness()
 
-    hive.plot_best_bee_path()
     hive.avg_fit_per_gen(num_generations, fitness_per_generation)
-
+    hive.plot_best_bee_path()
+    hive.create_genealogy_tree(min(5, num_generations))  # Use min to ensure it doesn't exceed the available generations
 
 if __name__ == "__main__":
-    num_generations = 100  # number of generations
+    num_generations = 10  # number of generations
     main(num_generations)
